9a487d4f674d673c2ba536a53c11173b4c7bbafb
Игорь Горбенко <gorbenkoigorek@mail.ru>
2023-05-12 20:26:32 +0300

    Created parser

--- custom_module.py
+++ custom_module.py
@@ -1,2 +1,8 @@
-def hui():
-    print('Volodya huisos')
+import numpy as np
+import pandas as pd
+from bs4 import BeautifulSoup
+import requests
+
+def get_tickers():
+    df = pd.read_csv("tickers_screener.csv")
+    return df['Symbol']
