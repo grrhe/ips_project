65af78096848f26074b2169d9a4e541429c50a8e
Игорь Горбенко <gorbenkoigorek@mail.ru>
2023-05-15 19:30:56 +0300

    Minor fix

--- custom_module.py
+++ custom_module.py
@@ -67,24 +67,75 @@ def parse_yf_esg(start=0, stop=len(get_tickers())):
 
 
 def parse_mc(tickers):
-    '''Функция возвращающая словарь из прибылей
+    '''Функция возвращающая словарь из прибылей и капитализации
     после парсинга значений с сайта Market cap'''
+    errors = {'year': []}
+    year_mod = {'2023': 2, '2022': 1, '2021': 0}
+    multp = {'млн': 10**6, 'млрд': 10**9, 'трлн': 10**12, 'тыс.': 10**3}
     profit = {
-        'Прибыль 2019': [],
-        'Прибыль 2020': [],
-        'Прибыль 2021': []
+        'Компания': [],
+        'Чистая прибыль 2019': [],
+        'Чистая прибыль 2020': [],
+        'Чистая прибыль 2021': [],
+        'Капитализация 2019': [],
+        'Капитализация 2020': [],
+        'Капитализация 2021': []
     }
-    for ticker in tickers:
+    for ticker in tqdm(tickers):
+        profit['Компания'].append(ticker)
         url = f'https://marketcap.ru/stocks/{ticker}/financial-statements/income-statement'
         headers = {'User-Agent': TOTALLY_NOT_A_PARSER}
         try:
             page = requests.get(url, headers=headers, timeout=10)
         except:
             page = requests.get(url, headers=headers, timeout=10)
+
         soup = BeautifulSoup(page.text, "html.parser")
-        a = soup.find_all(
-            'td',
-            class_='title-detail-table'
-        )
-        sleep(1)
-        return a
+        base_table = soup.find_all(
+            'table',
+            class_='table table-striped table-detail-stocks table-responsive table-responsive-sm table-responsive-md table-responsive-lg table-responsive-xl'
+        )[0]
+        try:
+            body = base_table.tbody.find_all('tr')
+            head = base_table.thead.tr
+            i = year_mod[head.find_all('th', class_='numeric-sort')[0].text]
+            resume = True
+        except:
+            errors['year'].append((ticker, head.find_all('th', class_='numeric-sort')[0].text))
+            resume = False
+
+        if resume:
+            pr = body[3].find_all('td')[i:3+i]
+            pr_values = []
+            for item in pr:
+                sep_data = item.text.split(sep=' ')
+                if len(sep_data) == 2:
+                    clear_data = float(sep_data[0])*multp[sep_data[1]]
+                    pr_values.append(clear_data)
+                else:
+                    pr_values.append(None)
+            profit['Чистая прибыль 2019'].append(pr_values[2])
+            profit['Чистая прибыль 2020'].append(pr_values[1])
+            profit['Чистая прибыль 2021'].append(pr_values[0])
+
+            cap = body[0].find_all('td')[i:3+i]
+            cap_values = []
+            for item in cap:
+                sep_data = item.text.split(sep=' ')
+                if len(sep_data) == 2:
+                    clear_data = float(sep_data[0])*multp[sep_data[1]]
+                    cap_values.append(clear_data)
+                else:
+                    cap_values.append(None)
+            profit['Капитализация 2019'].append(pr_values[2])
+            profit['Капитализация 2020'].append(pr_values[1])
+            profit['Капитализация 2021'].append(pr_values[0])
+        else:
+            profit['Чистая прибыль 2019'].append(None)
+            profit['Чистая прибыль 2020'].append(None)
+            profit['Чистая прибыль 2021'].append(None)
+            profit['Капитализация 2019'].append(None)
+            profit['Капитализация 2020'].append(None)
+            profit['Капитализация 2021'].append(None)
+        sleep(0.25)
+    return (profit, errors)
